bed7aeadc7875403abcd5e98491a0a19cdc9802c
AdinataKW <adinata2862@gmail.com>
2022-12-12 20:58:21 +0700

    tampilan activities

---
+++ resources/views/activities.blade.php
@@ -0,0 +1,34 @@
+@extends('layout.main')
+
+@section('container')
+<style>
+    body{
+        background-image:url(img/hotel4.jpg);
+        background-color:#cccccc;
+    }
+
+</style>
+
+
+<div class="text-light">
+    <h1>Activities</h1>
+    <h5>Home > Activities</h5>
+</div>
+
+<div class="col-xxl-8  d-flex ">
+        <img src="img/hotel2.jpg" class="img-fluid"  width="500" height="300">
+ 
+      
+<div class="col-lg-6 text-light ms-5">
+        <h1 class="display-5 fw-bold lh-1 mb-3">About Us</h1>
+        <p class="lead">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi incidunt temporibus iusto, perspiciatis labore similique commodi atque, aperiam quasi est magni neque accusantium sed esse voluptatum quod dignissimos deleniti aspernatur.</p>
+</div>
+</div>
+
+
+
+
+<footer class="container fixed-bottom text-light text-center">
+    <p>&copy;2022 Project Prakweb Laravel</p>
+</footer>
+@endsection
