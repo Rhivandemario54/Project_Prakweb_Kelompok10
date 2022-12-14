c2e5988ab57e3c4e0c6aa4ff02a7451240202c08
Rudi Haryanto <rudiharyantovrt99@gmail.com>
2022-12-14 18:48:43 +0700

    membuat halaman booking

---
+++ resources/views/booking.blade.php
@@ -0,0 +1,25 @@
+@extends('layouts.main')
+
+    @section('container')
+    <div class="container">
+        
+
+        <div class="container">
+
+
+        <div class="row g-0 ">
+            <div class="col-sm-6 col-md-8 bg-primary text-light">
+                <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat ipsam, placeat esse modi soluta, dolorum quo ipsum quas cum, cumque nam. Hic fuga pariatur accusamus eum et commodi id unde </h1> 
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque deleniti consectetur veritatis est similique quod? Maxime quia aliquid ullam illo officia, ex ipsa blanditiis itaque iusto nisi beatae sunt amet!
+                </p>
+            </div>
+            <div class="col-6 col-md-4 text-center text-primary mx-auto" style="width: 200px;">
+                <br>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Architecto amet sit ipsum debitis molestias cupiditate quae reprehenderit! Dolorem, molestias. Amet incidunt assumenda ipsam cumque facere iure quisquam? Distinctio, id perferendis.
+            </div>
+          </div>
+
+          
+      </div>
+    @endsection
